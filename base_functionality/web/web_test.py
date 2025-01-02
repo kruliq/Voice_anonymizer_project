a906de6c4fabb816a28adcef83d48e4ed37b6d66
tm_remote <kubakt@student.agh.edu.pl>
2025-01-02 11:46:50 +0000

    added static ip and port

--- base_functionality/web/web_test.py
+++ base_functionality/web/web_test.py
@@ -54,4 +54,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], secure_filename(filename))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
